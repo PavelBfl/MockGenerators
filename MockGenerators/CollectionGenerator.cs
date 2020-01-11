1aa7aa8c1ee3ac9bdb296753466adea5f2059740
PavelBfl <33580861+PavelBfl@users.noreply.github.com>
2020-01-11 19:10:04 +0500

    + Реализован генератор коллеций + Реализован генератор поставщик елементов коллекции

---
+++ MockGenerators/CollectionGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace MockGenerators
+{
+	/// <summary>
+	/// Генератор коллекций элементов
+	/// </summary>
+	/// <typeparam name="T">Тип элемента коллекции</typeparam>
+	public class CollectionGenerator<T> : ValueGenerator, IValueGenerator<IEnumerable<T>>
+	{
+		public CollectionGenerator(Random random, int lengthMin, IEnumerable<IValueGenerator<T>> itemsGenerators)
+			: base(random)
+		{
+			ItemsGenerators = (itemsGenerators ?? throw new NullReferenceException(nameof(itemsGenerators))).ToArray();
+
+			LengthMin = lengthMin <= ItemsGenerators.Count ? lengthMin : throw new ArgumentOutOfRangeException();
+		}
+		public CollectionGenerator(Random random, int lengthMin, int lengthMax, IValueGenerator<T> itemsGenerator)
+			: this(random, lengthMin, Enumerable.Range(0, lengthMax).Select(_ => itemsGenerator))
+		{
+
+		}
+		public CollectionGenerator(Random random, int lengthMax, IValueGenerator<T> itemsGenerator)
+			: this(random, 0, Enumerable.Range(0, lengthMax).Select(_ => itemsGenerator))
+		{
+
+		}
+
+		/// <summary>
+		/// Минимальная длина генерируемой коллекции
+		/// </summary>
+		public int LengthMin { get; } = 0;
+		/// <summary>
+		/// Генераторы елементов коллеции
+		/// </summary>
+		public IReadOnlyList<IValueGenerator<T>> ItemsGenerators { get; } = null;
+
+		public IEnumerable<T> Generate()
+		{
+			var result = new T[Random.Next(LengthMin, ItemsGenerators.Count)];
+			for (int i = 0; i < result.Length; i++)
+			{
+				result[i] = ItemsGenerators[i].Generate();
+			}
+			return result;
+		}
+	}
+}
---
+++ MockGenerators/ItemProviderGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MockGenerators
+{
+	/// <summary>
+	/// Генератор предоставляющий один из элементов коллекции
+	/// </summary>
+	/// <typeparam name="T">Тип генерируемого элемента</typeparam>
+	public class ItemProviderGenerator<T> : ValueGenerator, IValueGenerator<T>
+	{
+		private const string ITEMS_EMPTY_MESSAGE = "Предоставляемая коллекция пуста";
+
+		public ItemProviderGenerator(Random random, IEnumerable<T> items)
+			: base(random)
+		{
+			if (items is null)
+			{
+				throw new NullReferenceException(nameof(items));
+			}
+			if (!items.Any())
+			{
+				throw new InvalidOperationException(ITEMS_EMPTY_MESSAGE);
+			}
+
+			Items = items.ToArray();
+		}
+		/// <summary>
+		/// Контейнер предоставляемых элементов
+		/// </summary>
+		public IReadOnlyList<T> Items { get; } = null;
+
+		public T Generate()
+		{
+			return Items[Random.Next(Items.Count)];
+		}
+	}
+}
